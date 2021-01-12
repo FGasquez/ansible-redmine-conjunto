855d48b801691f7a31d201764e952feca279fea4
Manuel <manuchichi@hotmail.com>
2021-01-12 20:37:15 -0300

    First working version of Terraform with RDS

--- terraform/one-instance.tf
+++ terraform/one-instance.tf
@@ -13,29 +13,35 @@ provider "aws" {
 }
 
 variable "use_rds" {
-  default = false
+  type    = string
+  default = "false"
+}
+
+variable "localhost"{
+  type    = string
+  default = "localhost"
 }
 
 variable "root_user_password" {
-  type    = "string"
-  default = "ChangeMe"
+  type    = string
+  default = "ChangeMe12345"
 }
 
 variable "redmine_user_password" {
-  type    = "string"
-  default = "ChangeMe"
+  type    = string
+  default = "ChangeMe12345"
 }
 
 resource "aws_db_instance" "redmine-db" {
-  count = "${var.use_rds == true ? 1 : 0}"
+  count = var.use_rds == "true" ? 1 : 0
   
-  allocated_storage    = 2
+  allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   username             = "root"
-  password             = "${var.root_user_password}"
+  password             = var.root_user_password
   parameter_group_name = "default.mysql5.7"
 }
 
@@ -46,11 +52,16 @@ resource "aws_instance" "redmine-app" {
   key_name = "conjunto"
   
   provisioner "local-exec" {
-    command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ./conjunto.pem -i '${aws_instance.redmine-app.public_ip},' ../playbook.yml --extra-vars "run_mariadb=${!use_rds} mariadb_root_password=${var.root_user_password} mariadb_redmine_password=${var.redmine_user_password} mariadb_host=${var.use_rds == true ? ${aws_db_instance.redmine-db.address} : "localhost"}" "
+    command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ./conjunto.pem -i '${aws_instance.redmine-app.public_ip},' ../playbook.yml --extra-vars \"run_mariadb=${var.use_rds != "true"} mariadb_root_password=${var.root_user_password} mariadb_redmine_password=${var.redmine_user_password} mariadb_host=${var.use_rds == "true" ? aws_db_instance.redmine-db[0].address : var.localhost}\" "
     }
 }
 
-output "ip" {
-  value = aws_instance.example.public_ip
+output "redmine-ip" {
+  value = aws_instance.redmine-app.public_ip
 }
+
+output "database-ip" {
+  value = aws_db_instance.redmine-db[0].address
+}
+
 
