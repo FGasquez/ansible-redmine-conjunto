73bf77e3ad2dec97f1b58ba28aca4d8851849309
Manuel <manuchichi@hotmail.com>
2021-01-14 19:29:54 -0300

    Added restore database using variables

--- terraform/restore-database.sh
+++ terraform/restore-database.sh
@@ -1,4 +1,8 @@
-HOST=123
-DATABASE_HOST=123
+DATABASE_SNAPSHOT=123123
 
-ansible-playbook -u ubuntu --private-key ./conjunto.pem -i $HOST',' restore-scripts/update-database.yml --extra-vars 'database_host='$DATABASE_HOST'
+terraform apply -var="use_rds=true" -var="backup_snapshot=$DATABASE_SNAPSHOT"
+
+HOST=$(terraform output -raw redmine-ip)
+DATABASE_HOST=$(terraform output -raw database-ip)
+
+ansible-playbook -u ubuntu --private-key ./conjunto.pem -i $HOST, restore-scripts/update-database.yml --extra-vars "database_host=$DATABASE_HOST"
